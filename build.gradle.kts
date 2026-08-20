26f7e0e027aac9318ff221f861f800073b6cfafe
Tobias Weiss <tobias@tobias-weiss.org>
2026-08-20 18:32:23 +0200

    fix: remove dependencyResolutionManagement from project build.gradle.kts
    It belongs only in settings.gradle.kts; having it in the project
    script causes 'Unresolved reference: dependencyResolutionManagement'.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,3 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
